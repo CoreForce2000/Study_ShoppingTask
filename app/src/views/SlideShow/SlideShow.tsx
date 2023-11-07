185d21b4262dfab8ed65a019410f2b6d9cac7306
CoreForce2000 <46403786+CoreForce2000@users.noreply.github.com>
2023-11-07 11:35:18 +0100

    added new slide layout, and VAS

--- app/src/views/SlideShow/SlideShow.tsx
+++ app/src/views/SlideShow/SlideShow.tsx
@@ -1,18 +1,16 @@
 import React, { useState } from 'react';
-import slidesData from './slides.json';
 import styles from './SlideShow.module.css';
-import { SlideData } from './SlideShowInterface';
-import SlideViewPort from './components/Slide/Slide';
+import Slide from './components/Slide/Slide';
 import { CheckboxOption } from './SlideShowInterface';
 import Checkbox from './components/Checkbox/Checkbox';
+import VAS from './components/VAS/VAS';
 
 
 // SlideShow component to manage the slideshow state
 const SlideShow: React.FC = () => {
-  const [slides] = useState<SlideData[]>(slidesData);
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
   const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
-
+  const [selectedVAS, setSelectedVAS] = useState<number | null>(null);
 
   const [options, setOptions] = useState<CheckboxOption[]>([
     { label: 'several times a day', checked: false },
@@ -20,8 +18,26 @@ const SlideShow: React.FC = () => {
     { label: 'a few times a week', checked: false },
     { label: 'once a week', checked: false },
     { label: 'once a month or less', checked: false },
-    { label: 'very rarely / not at all', checked: false },
+    { label: 'very rarely / not at all', checked: false }
   ]);
+
+  const slides = [
+    <Slide backgroundImage={`/src/assets/slides/Phase1/Slide1.png`} >
+          <div style={{width:"100%", padding:"7%", display:"flex", justifyContent:"left"}}>
+            <div style={{backgroundColor:"white", width:"100%", marginTop:"20%"}}> 
+              <Checkbox options={options} setOptions={setOptions}/>
+            </div>
+          </div>
+    </Slide>,
+
+    <Slide backgroundImage={`/src/assets/slides/Phase1/Slide2.png`} >
+      <div style={{width:"100%", padding:"7%", display:"flex", justifyContent:"left"}}>
+        <div style={{backgroundColor:"white", width:"100%", marginTop:"20%"}}>
+          <VAS minLabel='Not at all' maxLabel='Very much' setValue={setSelectedVAS} />
+        </div>
+      </div>
+    </Slide>
+  ]
 
   const currentSlide = slides[currentSlideIndex];
 
@@ -34,17 +50,11 @@ const SlideShow: React.FC = () => {
   };
   
 
-  // Render the current slide https://open.spotify.com/intl-de/track/4lEDVWeadQqpJElNbPMec1?si=cf56f88f9e634c3e
+  // Render the current slide
   return (
     <div className={styles.slideShow}>
       {currentSlide && (
-        <SlideViewPort backgroundImage={'/src/assets/slides/phase1/Slide1.png'} >
-          <div style={{width:"100%", padding:"7%", display:"flex", justifyContent:"left"}}>
-            <div style={{backgroundColor:"white", width:"100%", marginTop:"20%"}}> 
-              <Checkbox options={options} setOptions={setOptions}/>
-            </div>
-          </div>
-        </SlideViewPort>
+        currentSlide
       )}
       <button onClick={goToPreviousSlide} disabled={currentSlideIndex === 0}>
         Previous
---
+++ app/src/views/SlideShow/components/VAS/VAS.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import styles from './VAS.module.css';
+
+interface VASProps {
+  minLabel: string;
+  maxLabel: string;
+  setValue: (value: number) => void; // Function to set the value in the parent state
+}
+
+const VAS: React.FC<VASProps> = ({ minLabel, maxLabel, setValue }) => {
+    const [indicatorPosition, setIndicatorPosition] = useState<number | null>(null);
+
+    const handleVASClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+        const x = e.clientX - rect.left; // Click position relative to the line
+        const width = rect.width; // Width of the line container
+        const value = (x / width) * 100; // Convert the click position to a value from 0 to 100
+
+        setIndicatorPosition(x); // Update the indicator's position
+        setValue(value); // Update the value in the parent component
+    };
+
+    return (
+        <div className={styles.rangeSlider}>
+            <div className={styles.rangeSliderInner}>
+                <span className={styles.label}>{minLabel}</span>
+                <div className={styles.vasContainer} onClick={handleVASClick}>
+                    <div className={styles.line}></div>
+                    {indicatorPosition !== null && (
+                        <div
+                            className={styles.indicator}
+                            style={{ left: `${indicatorPosition}px` }} // Position the indicator
+                        ></div>
+                    )}
+                </div>
+                <span className={styles.label}>{maxLabel}</span>
+            </div>
+        </div>
+    );
+};
+
+export default VAS;
